fafa711cd4bf6dea6ae7614cdcc63dcd0cc9e365
Luis Saucedo <luigisaucedo.ls@gmail.com>
2022-05-26 10:41:32 -0400

    Moviendo los Types y Resolvers hacia su propio archivo

--- index.js
+++ index.js
@@ -1,51 +1,7 @@
-const { ApolloServer, gql } = require('apollo-server');
+const { ApolloServer } = require('apollo-server');
+const typeDefs = require('./db/schema');
+const resolvers = require('./db/resolvers');
 
-
-
-// Schema
-const typeDefs = gql`
-
-    type Curso {
-        titulo: String
-    }
-
-    type Tecnologia {
-        tecnologia: String
-    }
-
-    type Query {
-        obtenerCursos : [Curso]
-        obtenerTecnologia: [Tecnologia]
-    }
-
-`;
-
-const cursos = [
-    {
-        titulo: 'JavaScript Moderno Guía Definitiva Construye +10 Proyectos',
-        tecnologia: 'JavaScript ES6',
-    },
-    {
-        titulo: 'React - La Guía Completa: Hooks Context Redux MERN +15 Apps',
-        tecnologia: 'React',
-    },
-    {
-        titulo: 'Node.js - Bootcamp Desarrollo Web inc. MVC y REST APIs',
-        tecnologia: 'Node.js'
-    }, 
-    {
-        titulo: 'ReactJS Avanzado - FullStack React GraphQL y Apollo',
-        tecnologia: 'React'
-    }
-];
-
-// Resolvers
-const resolvers = {
-    Query: {
-        obtenerCursos: () => cursos,
-        obtenerTecnologia: () => cursos
-    }
-}
 
 // Servidor
 const server = new ApolloServer({
