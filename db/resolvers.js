fa8b1265d6b5b3594b5e4861e11ccb957962d57f
Luis Saucedo <luigisaucedo.ls@gmail.com>
2022-05-30 08:50:22 -0400

    Obtener mejores: Clientes

--- db/resolvers.js
+++ db/resolvers.js
@@ -113,6 +113,27 @@ const resolvers = {
         const pedidos = await Pedido.find({ vendedor: ctx.usuario.id, estado: estado});
 
         return pedidos;
+      },
+
+      mejoresClientes: async() => {
+        const clientes = await Pedido.aggregate([
+          { $match : { estado : "COMPLETADO" } },
+          { $group : {
+            _id: "$cliente",
+            total: { $sum : "$total" }
+          }},
+          {
+            $lookup: {
+              from: "clientes",
+              localField: "_id",
+              foreignField: "_id",
+              as: "cliente"
+            }
+          },
+          {
+            $sort : { total : -1}
+          }
+        ])
       }
 
     },
